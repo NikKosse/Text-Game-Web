4f962ae3d990e718db388ac2b854c1e2872cb7e4
nikolas <nikolas.kosse@gmail.com>
2018-03-05 21:05:52 -0600

    deleted TextGameFrontEnd after updating the Angular CLI and remade the project to be using the most recent version of Angular.  Added a WebServie but have not hooked it up to the backend yet

--- TextGameFrontEnd/e2e/app.e2e-spec.ts
+++ TextGameFrontEnd/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { TextGameFrontEndPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('text-game-front-end App', function() {
-  let page: TextGameFrontEndPage;
+describe('text-game-front-end App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new TextGameFrontEndPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
